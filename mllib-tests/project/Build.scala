72b82fa6a92d770a6ba12b38c191c7397b4413b6
Burak <brkyvz@gmail.com>
2014-08-17 16:59:34 -0700

    Build.scala modified

--- mllib-tests/project/Build.scala
+++ mllib-tests/project/Build.scala
@@ -3,25 +3,25 @@ import Keys._
 
 object MyBuild extends Build {
 
-    lazy val onepointone = project.in(file("onepointone")).
-        settings(
+    lazy val root = project.in(file(".")).aggregate(onepointoh,onepointone).
+      settings(
+        libraryDependencies ++= Seq(
+          "net.sf.jopt-simple" % "jopt-simple" % "4.5",
+          "org.scalatest" %% "scalatest" % "2.2.1" % "test",
+          "org.apache.spark" %% "spark-mllib" % "1.0.0" % "provided",
+          "org.slf4j" % "slf4j-log4j12" % "1.7.2"
+        )
+      ).dependsOn(onepointoh, onepointone)
+
+    lazy val onepointone = project
+        .settings(
           libraryDependencies += "net.sf.jopt-simple" % "jopt-simple" % "4.5",
           libraryDependencies += "org.apache.spark" %% "spark-mllib" % "1.1.1-SNAPSHOT" % "provided"
         ).dependsOn(onepointoh)
 
-    lazy val onepointoh = project.in(file("onepointoh")).
-          settings(        //should be set to 1.0.0 or higher
+    lazy val onepointoh = project
+          .settings(        //should be set to 1.0.0 or higher
             libraryDependencies += "org.apache.spark" %% "spark-mllib" % "1.0.0" % "provided",
             libraryDependencies += "net.sf.jopt-simple" % "jopt-simple" % "4.5" 
           )
-
-   lazy val root = project.in(file(".")).
-      settings(
-        libraryDependencies ++= Seq(
-            "net.sf.jopt-simple" % "jopt-simple" % "4.5",
-            "org.scalatest" %% "scalatest" % "2.2.1" % "test",
-            "org.slf4j" % "slf4j-log4j12" % "1.7.2"
-           )
-        ).dependsOn(onepointoh, onepointone)
-
 }
